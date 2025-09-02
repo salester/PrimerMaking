1d78b8199730be8dcf767bf620280e290c81afce
salester <51415849+salester@users.noreply.github.com>
2025-09-02 00:48:42 -0400

    Update Renumbering for Primer Making.r
    Convert Ensembl 104 rsIDs to current Ensembl version for accurate consequence assessment

--- Renumbering for Primer Making.r
+++ Renumbering for Primer Making.r
@@ -1,25 +1,41 @@
 #Mapping Variant Loci to Exclude in Primer Making
 #Created by Sarah Lester
-#Modified 02-14-23
+#Modified 09-01-2025
 
 #Example files can be found in GitHub repository
 
 if (!require(tidyverse)) install.packages("tidyverse")
+if (!require(biomaRt)) BiocManager::install("biomaRt")
 library(tidyverse)
+library(biomaRt)
 
 # VARIABLES YOU NEED TO CHANGE
 # File paths
-working_directory_path <- "Working/Directory/Here" # Make this the folder where all of your input files are and where you want your output files to go
-sequence_raw_path <- "Sequence_From_UCSCGenomeBrowser.txt" # Make sure this is a .txt file and in your working directory
-variant_locations_path <- "Variant_Locations.txt" # Make sure this is a .txt file and in your working directory
+working_directory_path <- "working/directory/here/" # Make this the folder where all of your input files are and where you want your output files to go
+sequence_raw_path <- "Gene_Sequence.txt" # Make sure this is a .txt file and in your working directory
+variant_locations_path <- "Gene_Variant_Locations.csv" # Make sure this is a .csv file and in your working directory
 
 # Genomic Coordinates - If on the positive strand (transcribed left to right on genome browser), start has a lower number than stop
 #                       If on the negative strand (transcribed right to left on genome browser), start has a higher number than stop
-start <- 108811175
-stop <- 108841609
+start <- NUM_HERE
+stop <- NUM_HERE
+
+# Consequences of Interest - Can be changed if necessary
+consequences <- c("coding_sequence_variant", 
+                  "frameshift_variant",  
+                  "inframe_deletion",
+                  "inframe_insertion",
+                  "missense_variant",
+                  "synonymous_variant",
+                  "start_lost", 
+                  "start_retained_variant",
+                  "stop_gained",
+                  "stop_lost",
+                  "stop_retained_variant")
 
 # THIS SECTION SHOULD REMAIN UNCHANGED
 setwd(working_directory_path)
+geneName <- str_split_i(sequence_raw_path, "_", 1)
 
 #Read in sequence txt file with sequence copy-pasted from the genome browser with header "Sequence"
 sequence_raw <- data.frame(read.delim(sequence_raw_path), stringsAsFactors = FALSE)
@@ -55,10 +71,42 @@ sequence_junctions[junctions]  <- "J"
 sequence_trimmed <- sequence_junctions[which(uppers == 1)]
 
 #Read in list of variant locations
-variants <- read.delim(variant_locations_path)
+variants <- read.csv(variant_locations_path)
+
+### START TEST ###
+# Verify that consequences match those in Ensembl 104
+# Retaining for additional testing
+
+# ensembl104 <- useEnsembl(biomart = "snps", version = 104, dataset = "mmusculus_snp")
+# 
+# forVerification <- getBM(attributes = c("refsnp_id", "chrom_start", "chrom_end", "consequence_type_tv", "snp"),
+#                          filters = "snp_filter",
+#                          values = variants$SNP.ID,
+#                          mart = ensembl104)
+# 
+# test <- variants %>% left_join(forVerification, by = join_by(SNP.ID == refsnp_id))
+
+## On first glance it looks like yes, at least for this gene they match
+### END TEST ###
+
+# Get updated annotation from current ensembl version
+ensembl <- useEnsembl(biomart = "snps", dataset = "mmusculus_snp")
+
+newAnno <- getBM(attributes = c("refsnp_id", "chrom_start", "consequence_type_tv"),
+                 filters = "snp_filter",
+                 values = variants$SNP.ID,
+                 mart = ensembl)
+
+# Join new annotation, 
+# filter for consequences of interest in most current ensembl version,
+# Rename chrom_start (most current position) for use in rest of script
+variants_newAnno <- variants %>% full_join(newAnno, by = join_by(SNP.ID == refsnp_id)) %>% 
+                                 filter(consequence_type_tv %in% consequences) %>% 
+                                 rename(Position = chrom_start) %>% 
+                                 mutate()
 
 #Use variant list to change snps from base to "SNP" to easily see where they are
-sequence_trimmed[names(sequence_trimmed) %in% variants$Position] <- "SNP"
+sequence_trimmed[names(sequence_trimmed) %in% variants_newAnno$Position] <- "SNP"
 
 #Renumber sequence to reflect length, not genomic position
 sequence_renumber <- sequence_trimmed
@@ -77,8 +125,7 @@ to_avoid <- paste(to_avoid, collapse = ", ")
 
 #Output a CSV with all relevant information
 #This is all just to make the final document easier to read
-geneName <- unique(variants$Gene)
-results <- data.frame(Result = c("Sequence For IDT", "Excluded Region List/SNPs to Avoid", "Target Region List/Exon Junctions"),
+results <- data.frame(Result = c("Sequence For IDT", "Excluded Region List/SNPs to Avoid", "Overlap Junction List"),
                         Data = c(sequenceForIDT, to_avoid, junctions))
 
 results <- results %>% add_row(Result = "", Data = "", .after = 1)
